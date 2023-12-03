c66616d74735b90896d9f675ca5d7ad29bd69c86
nabilproyek <m.nabilallatif4@gmail.com>
2023-12-03 21:20:30 +0700

    halo

--- start.sh
+++ start.sh
@@ -1,11 +1,18 @@
 set -eu
+
 export PYTHONUNBUFFERED=true
+
 VIRTUALENV=.data/venv
-if [ -d SVIRTUALENV ]; then python3 -m venv SVIRTUALENV
+
+if [ ! -d $VIRTUALENV ]; then 
+    python3 -m venv $VIRTUALENV
 fi
-if [ -f $VIRTUALENV/bin/pip ]; then
-curl --silent --show-error-retry 5 https://bootstrap.pypa.io/get-pip.py | SVIRTUALE
+
+if [ ! -f $VIRTUALENV/bin/pip ]; then
+    curl --silent --show-error --retry 5 https://bootstrap.pypa.io/get-pip.py | $VIRTUALE
 fi
+
 $VIRTUALENV/bin/pip install -r requirements.txt
-SVIRTUALENV/bin/python3 app.py
+
+$VIRTUALENV/bin/python3 app.py
 Footer
